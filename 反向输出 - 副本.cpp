3d28f9eb34829bdb3262cb3df2530da5f7c41d47
yanyangtian1106 <1035282452@qq.com>
2022-01-26 17:26:30 +0800

    new1

---
+++ 反向输出 - 副本.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    scanf("%1d%1d%1d%1d", &a, &b, &c, &d);
+    printf("%d%d%d%d", d, c, b, a);
+    return 0;
+}
